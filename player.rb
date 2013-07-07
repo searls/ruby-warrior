32d610f4239f30f9c6784c0ed315bbb6234e040b
Justin Searls <searls@gmail.com>
2013-07-06 23:39:35 -0400

    refactor a bit. sick of passing warrior everywhere

--- player.rb
+++ player.rb
@@ -2,38 +2,38 @@ module WarriorQueries
   MAX_HP = 20
   DIRECTIONS = [:forward, :left, :right, :backward]
 
-  def restable?(warrior)
-    warrior.health < MAX_HP && enemy_possibly_exists?(warrior)
+  def restable?
+    warrior.health < MAX_HP && enemy_possibly_exists?
   end
 
-  def attackable_direction(warrior)
-    direction_of(warrior, :enemy)
+  def attackable_direction
+    direction_of(:enemy)
   end
 
-  def attackable_bearing(warrior)
-    non_stairway_bearing_of(warrior, :enemy)
+  def attackable_bearing
+    non_stairway_bearing_of(:enemy)
   end
 
   # If there are multiple unbound enemies surrounding me
   # Then I want to bind the ones in the direction I do not wish to travel
-  def bindable_direction(warrior)
-    unbound_enemies = directions_for(warrior).select do |dir|
+  def bindable_direction
+    unbound_enemies = directions.select do |dir|
       feel = warrior.feel(dir)
       feel.enemy? && !feel.captive?
     end
     return unbound_enemies.last if unbound_enemies.size > 1
   end
 
-  def rescuable_direction(warrior)
-    direction_of(warrior, :captive)
+  def rescuable_direction
+    direction_of(:captive)
   end
 
-  def rescuable_bearing(warrior)
-    non_stairway_bearing_of(warrior, :captive)
+  def rescuable_bearing
+    non_stairway_bearing_of(:captive)
   end
 
-  def uncleared_map_bearing(warrior)
-    if anything_interesting?(warrior) && warrior.feel(warrior.direction_of_stairs).stairs?
+  def uncleared_map_bearing
+    if anything_interesting? && adjacent_to_stairs?
       DIRECTIONS.shuffle.find do |dir|
         feel = warrior.feel(dir)
         feel.empty? && !feel.stairs?
@@ -41,51 +41,55 @@ module WarriorQueries
     end
   end
 
-  def cleared_map_bearing(warrior)
+  def cleared_map_bearing
     warrior.direction_of_stairs
   end
 
   private
 
-  def directions_for(warrior)
+  def adjacent_to_stairs?
+    warrior.feel(warrior.direction_of_stairs).stairs?
+  end
+
+  def directions
     ([warrior.direction_of_stairs] + DIRECTIONS).uniq
   end
 
-  def direction_of(warrior, space_type)
-    directions_for(warrior).find do |dir|
+  def direction_of(space_type)
+    directions.find do |dir|
       warrior.feel(dir).send("#{space_type}?")
     end
   end
 
-  def non_stairway_bearing_of(warrior, space_type)
-    bearings_of(warrior, space_type).find { |dir| !warrior.feel(dir).stairs? }
+  def non_stairway_bearing_of(space_type)
+    bearings_of(space_type).find { |dir| !warrior.feel(dir).stairs? }
   end
 
-  def bearings_of(warrior, space_type)
+  def bearings_of(space_type)
     warrior.listen.select(&("#{space_type}?".to_sym)).
       map {|space| warrior.direction_of(space) }
   end
 
-  def bearing_of(warrior, space_type)
-    bearings_of(warrior, space_type).first
+  def bearing_of(space_type)
+    bearings_of(space_type).first
   end
 
-  def anything_interesting?(warrior)
+  def anything_interesting?
     warrior.listen.any? do |space|
       space.captive? || space.enemy?
     end
   end
 
-  def enemy_possibly_exists?(warrior)
-    any_non_captive_units?(warrior) || exists_but_only_at_a_distance?(warrior, :enemy)
+  def enemy_possibly_exists?
+    any_non_captive_units? || exists_but_only_at_a_distance?(:enemy)
   end
 
-  def any_non_captive_units?(warrior)
+  def any_non_captive_units?
     warrior.listen.any? {|space| space.unit && !space.unit.is_a?(RubyWarrior::Units::Captive) }
   end
 
-  def exists_but_only_at_a_distance?(warrior, space_type)
-    !direction_of(warrior, space_type) && bearing_of(warrior, space_type)
+  def exists_but_only_at_a_distance?(space_type)
+    !direction_of(space_type) && bearing_of(space_type)
   end
 end
 
@@ -98,7 +102,7 @@ class ActMaybe
   end
 
   def act
-    if result = @query.call(@warrior)
+    if result = @query.call
       take_action(@warrior.method(@command), result)
       return true
     else
@@ -113,7 +117,6 @@ class ActMaybe
 
     method.call(*args.shift(method.arity.abs))
   end
-
 end
 
 module WarriorCommands
@@ -126,6 +129,7 @@ end
 class Player
   include WarriorQueries
   include WarriorCommands
+  attr_reader :warrior
 
   ACTIONS = {
     bindable_direction: :bind!,
@@ -142,6 +146,8 @@ class Player
   ]
 
   def play_turn(warrior)
+    @warrior = warrior
+
     ACTIONS.each do |(query, command)|
       return if act(warrior, query, command)
     end
